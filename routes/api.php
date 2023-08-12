da22090990d89d5eb3a3d01a836355a16644c5cb
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-08-13 01:02:12 +0300

    added routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\IncomeController;
 
-
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     // Get all income records for the authenticated user
