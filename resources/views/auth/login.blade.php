376bad9d9eac751dbcd3f4eed46475d3bfcc325a
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-08-12 12:06:16 +0300

    fixed login and regster

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,155 +1,184 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Mapato | Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mapato | Login</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css')}}">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css')}}">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css')}}">
-  <style>
-    body {
-  background-image: url('back.jpg');
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
+    <style>
+        body {
+            background-image: url('back.jpg');
 
-}
-
-  </style>
+        }
+    </style>
 </head>
-<body style="  background-image: url('{{ asset('back.jpg') }}');  background-position: center;
+
+<body
+    style="  background-image: url('{{ asset('back.jpg') }}');  background-position: center;
 background-repeat: no-repeat;
 background-size: cover;">
     <!-- Navbar -->
     <nav class="navbar navbar-expand bg-navy navbar-light">
         <!-- Left navbar links -->
-        <b><h3 ><a href="index.php" class="text-light"><b><i class="text-danger">M</i><i>apato</i><i class="text-danger">.</i></b></a></h3></b>
+        <b>
+            <h3><a href="index.php" class="text-light"><b><i class="text-danger">M</i><i>apato</i><i
+                            class="text-danger">.</i></b></a></h3>
+        </b>
 
-</nav>    
+    </nav>
 
-<!-- login form -->
-<div class="container mt-5">
-    <div class="row">
-    <div class="col-md-6 float-left mt-5 " >
-              <h1 class="text-danger text-center"><b> About <i class="text-white"> Mapato.</i> <hr class="bg-light"></b></h1>
-              <div class="text-light bg-navy p-4" style="border-radius:5%;">
-                <h4 class=" text-light " >Welcome to <b><i class="text-danger"> Mapato </i></b>.This is a place where you can easily :</h4>
-                <ul type="none">
-                  <li><h5 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger" style="border-radius:100%;"></i> Monitor Your Budget</h5> </li>
-                  <li><h5 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger" style="border-radius:100%;"></i> Monitor Daily Expenses</h5> </li>
-                  <li><h5 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger" style="border-radius:100%;"></i> Monitor Daily Earnings</h5> </li>
-                  <li><h5 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger" style="border-radius:100%;"></i> Monitor Daily Savings</h5> </li>
-                  <li><h5 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger" style="border-radius:100%;"></i> Monitor Your Debts</h5> </li>
-                </ul>
-              </div>
-        </div>
-        <div class="col-md-6 float-right mt-5 ">
-            <div class="card card-navy opacity-25" style="opacity:94%;">
-                <div class="card-header">
-                  <h3 class="card-title"></h3>
+    <!-- login form -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 float-left mt-4 ">
+                <h1 class="text-danger text-center"><b> About <i class="text-white"> Mapato.</i>
+                        <hr class="bg-light">
+                    </b></h1>
+                <div class="text-light bg-navy p-4" style="border-radius:5%;">
+                    <h4 class=" text-light ">Welcome to <b><i class="text-danger"> Mapato </i></b>.This is a place where
+                        you can easily :</h4>
+                    <ul type="none">
+                        <li>
+                            <h6 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger"
+                                    style="border-radius:100%;"></i> Monitor Your Budget</h6>
+                        </li>
+                        <li>
+                            <h6 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger"
+                                    style="border-radius:100%;"></i> Monitor Daily Expenses</h6>
+                        </li>
+                        <li>
+                            <h6 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger"
+                                    style="border-radius:100%;"></i> Monitor Daily Earnings</h6>
+                        </li>
+                        <li>
+                            <h6 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger"
+                                    style="border-radius:100%;"></i> Monitor Daily Savings</h6>
+                        </li>
+                        <li>
+                            <h6 class="text-white "><i class="fa fa-arrow-right bg-danger p-1 text-danger"
+                                    style="border-radius:100%;"></i> Monitor Your Debts</h6>
+                        </li>
+                    </ul>
                 </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form method="POST" action="{{ route('login') }}">
-                  @csrf
-                  <div class="card-body">
-                    <div class="form-group">
-                      <label for="exampleInputEmail1"><i class="fa fa-envelope"></i> Email</label>
-                      <input  class="form-control" id="exampleInputEmail1"type="email" name="email" :value="old('email')" placeholder="Enter Email" required autofocus autocomplete="username" required>
+            </div>
+            <div class="col-md-6 float-right mt-5 ">
+                <div class="card card-navy opacity-25" style="opacity:94%;">
+                    <div class="card-header">
+                        <h3 class="card-title"></h3>
                     </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1"><i class="fa fa-lock"></i> Password</label>
-                      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password" required>
-                    </div>
-                    <div class="form-group">
-                      
-                      <input type="checkbox" name="" class="form" id="remember" placeholder="Password">
-                      <label for="remember">Remember Me</label>
-                      
-                    </div>
-                    <?php
-                  if (isset($_GET["request"])){
-                  ?>
-                <p class="btn btn-danger btn-block disabled mx-auto " style="margin-left:20px;">wrong username or password</p>
-                  <?php     
-                }
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1"><i class="fa fa-envelope"></i> Email</label>
+                                <input class="form-control" id="exampleInputEmail1"type="email" name="email"
+                                    :value="old('email')" placeholder="Enter Email" required autofocus
+                                    autocomplete="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1"><i class="fa fa-lock"></i> Password</label>
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                                    placeholder="Enter Password" required>
+                            </div>
+                            <div class="form-group">
 
-                ?>
-                  </div>
-                  <!-- /.card-body -->
-                  <div class="card-footer">
-                    <button type="submit" name="login" class="btn btn-danger">Log In</button>
-                    <a href="{{ url('/register')}}" class="btn bg-navy">Create Account</a>
-                   
-                  </div>
+                                <input type="checkbox" name="" class="form" id="remember"
+                                    placeholder="Password">
+                                <label for="remember">Remember Me</label>
 
-                </form>
-              </div>
-              <!-- /.card -->
+                            </div>
+                            {{-- displaying all the errors  --}}
+                            @if ($errors->any())
+                                <div class="btn disabled btn-danger col-12 mb-2">
+                                    @foreach ($errors->all() as $error)
+                                        <div>{{ $error }}</div>
+                                    @endforeach
+                                </div>
+                            @endif
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" name="login" class="btn btn-danger">Log In</button>
+                            <a href="{{ url('/register') }}" class="btn bg-navy">Create Account</a>
+
+                        </div>
+
+                    </form>
+                </div>
+                <!-- /.card -->
+            </div>
+
         </div>
-        
     </div>
-</div>
 
-<footer class="fixed-bottom bg-navy text-center">
-  <br>
-    <strong>Copyright &copy; @php  echo date('Y'); @endphp <a href="" class="text-danger">TazarChriss</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
- 
-    </div>
-<h1></h1>
-  </footer>
-<!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js')}}"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- ChartJS -->
-<script src="{{ asset('plugins/chart.js/Chart.min.js')}}"></script>
-<!-- Sparkline -->
-<script src="{{ asset('plugins/sparklines/sparkline.js')}}"></script>
-<!-- JQVMap -->
-<script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{ asset('plugins/moment/moment.min.js')}}"></script>
-<script src="{{ asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-<!-- Summernote -->
-<script src="{{ asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('dist/js/adminlte.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('dist/js/pages/dashboard.js')}}"></script>
+    <footer class="fixed-bottom bg-navy text-center">
+        <br>
+        <strong>Copyright &copy; @php  echo date('Y'); @endphp <a href="" class="text-danger">TazarChriss</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+
+        </div>
+        <h1></h1>
+    </footer>
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- ChartJS -->
+    <script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset('plugins/sparklines/sparkline.js') }}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+    <script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+    <!-- daterangepicker -->
+    <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+    <!-- Summernote -->
+    <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
+    <!-- overlayScrollbars -->
+    <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('dist/js/demo.js') }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
 </body>
-</html>
 
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,177 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mapato | Login</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('plugins/jqvmap/jqvmap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.min.css') }}">
+    <style>
+        body {
+            background-image: url('back.jpg');
+
+        }
+    </style>
+</head>
+
+<body
+    style="  background-image: url('{{ asset('back.jpg') }}');  background-position: center;
+background-repeat: no-repeat;
+background-size: cover;">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand bg-navy navbar-light">
+        <!-- Left navbar links -->
+        <b>
+            <h3><a href="index.php" class="text-light"><b><i class="text-danger">M</i><i>apato</i><i
+                            class="text-danger">.</i></b></a></h3>
+        </b>
+
+    </nav>
+
+    <!-- login form -->
+    <div class="container mt-5">
+        <div class="row">
+         
+            <div class="col-md-8 mx-auto mt-5 ">
+                <div class="card card-navy opacity-25" style="opacity:94%;">
+                    <div class="card-header">
+                        <h3 class="card-title"></h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputEmail1"> First Name</label>
+                                <input type="text" name="fname" class="form-control" id="exampleInputEmail1"
+                                    placeholder="Enter First Name" required>
+                            </div>
+
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputEmail1"> Last Name</label>
+                                <input type="text" name="lname" class="form-control" id="exampleInputEmail1"
+                                    placeholder="Enter Last Name" required>
+                            </div>
+
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputEmail1"> Username</label>
+                                <input type="text" name="name" class="form-control" id="exampleInputEmail1"
+                                    placeholder="Enter Username" required>
+                            </div>
+
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputEmail1"> Email</label>
+                                <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                                    placeholder="Enter Email" required>
+                            </div>
+
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputPassword1">New Password</label>
+                                <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                                    placeholder="Enter Password" required>
+                            </div>
+
+                            <div class="form-group col-md-6 float-left">
+                                <label for="exampleInputPassword1">Confirm Password</label>
+                                <input type="password" name="password_confirmation" class="form-control"
+                                    id="exampleInputPassword1" placeholder="Enter Password" required>
+                            </div>
+                            <div class="form-group">
+
+                                <input type="checkbox" name="" class="form" id="remember" required>
+                                <label for="remember"><a href="">Terms and Conditions</a> </label>
+
+                            </div>
+                            <?php
+                            if (isset($_GET["request"])) {
+                            ?>
+                                                <p class="btn btn-danger btn-block disabled mx-auto " style="margin-left:20px;">wrong
+                                                    username or password</p>
+                                                <?php
+                            }
+
+                            ?>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" name="register" class="btn btn-danger">Create Account</button>
+                            <a href="{{ url('/') }}" class="btn bg-navy">Log In</a>
+                        </div>
+
+                    </form>
+                </div>
+                <!-- /.card -->
+            </div>
+
         </div>
+    </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <footer class="fixed-bottom bg-navy text-center">
+        <br>
+        <strong>Copyright &copy; @php  echo date('Y'); @endphp <a href="" class="text-danger">TazarChriss</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+
         </div>
+        <h1></h1>
+    </footer>
+    <!-- jQuery -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- ChartJS -->
+    <script src="{{ asset('plugins/chart.js/Chart.min.js') }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset('plugins/sparklines/sparkline.js') }}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+    <script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+    <!-- daterangepicker -->
+    <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+    <!-- Summernote -->
+    <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
+    <!-- overlayScrollbars -->
+    <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('dist/js/adminlte.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('dist/js/demo.js') }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
+</body>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
