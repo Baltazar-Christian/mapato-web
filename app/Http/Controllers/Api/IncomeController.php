5c9b7064b78388e3a216090f1731601a8a3c35af
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-11-03 21:21:25 +0300

    okay

--- app/Http/Controllers/Api/IncomeController.php
+++ app/Http/Controllers/Api/IncomeController.php
@@ -6,10 +6,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Income;
 use Illuminate\Http\Request;
-use Laravel\Sanctum\Facades\Auth;
-
-// use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Auth;
 
 class IncomeController extends Controller
 {
@@ -61,9 +58,9 @@ class IncomeController extends Controller
     public function update(Request $request, Income $income)
     {
         // Check if the income record belongs to the authenticated user
-        if ($income->user_id !== Auth::id()) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+        // if ($income->user_id !== Auth::id()) {
+        //     return response()->json(['error' => 'Unauthorized'], 401);
+        // }
 
         // Validate the request data
         $request->validate([
@@ -85,9 +82,9 @@ class IncomeController extends Controller
     public function destroy(Income $income)
     {
         // Check if the income record belongs to the authenticated user
-        if ($income->user_id !== Auth::id()) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+        // if ($income->user_id !== Auth::id()) {
+        //     return response()->json(['error' => 'Unauthorized'], 401);
+        // }
 
         // Delete the income record
         $income->delete();
